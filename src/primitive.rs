5d0cc784917b94d7d282dcde41de122bfc689e3a
miso-devel <kengo279@icloud.com>
2023-05-04 18:05:17 +0900

    thread

--- src/primitive.rs
+++ src/primitive.rs
@@ -1,4 +1,5 @@
 pub mod array;
 pub mod object;
 pub mod string;
+pub mod thread;
 pub mod types;
---
+++ src/primitive/thread.rs
@@ -0,0 +1,32 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+fn main() {
+    // スレッドの複数立ち上げ
+    // let mut handlers = Vec::new();
+    // for x in 0..=10 {
+    //     handlers.push(thread::spawn(move || {
+    //         println!("Hello-world:{}", x);
+    //     }))
+    // }
+    // for handler in handlers {
+    //     let _ = handler.join();
+    // }
+
+    // 各スレッドでのメモリ共有
+    let mut multiple_hundler = Vec::new();
+    let share_data = Arc::new(Mutex::new(vec![1; 10]));
+    for x in 0..10 {
+        let cloned_share_data = share_data.clone();
+        multiple_hundler.push(thread::spawn(move || {
+            cloned_share_data.lock().unwrap()[x] += x;
+        }))
+    }
+
+    for handler in multiple_hundler {
+        let _ = handler.join();
+    }
+    println!("{:?}", share_data)
+}
